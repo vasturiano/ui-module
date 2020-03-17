9d2c5f7f81f25cf9cdf47a7801737ceb637f7a09
vasco <vastur@gmail.com>
2020-03-17 01:16:47 -0700

    Refactor rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -4,28 +4,65 @@ import postCss from 'rollup-plugin-postcss';
 import postCssSimpleVars from 'postcss-simple-vars';
 import postCssNested from 'postcss-nested';
 import babel from 'rollup-plugin-babel';
-import { name, homepage, version } from './package.json';
+import { terser } from "rollup-plugin-terser";
+import { name, homepage, version, dependencies } from './package.json';
 
-export default {
-  input: 'src/index.js',
-  output: [
-    {
-      format: 'umd',
-      name: '__my_global_var_name__',
-      file: `dist/${name}.js`,
-      sourcemap: true,
-      banner: `// Version ${version} ${name} - ${homepage}`
-    }
-  ],
-  plugins: [
-    postCss({
-      plugins: [
-        postCssSimpleVars(),
-        postCssNested()
-      ]
-    }),
-    babel({ exclude: 'node_modules/**' }),
-    resolve(),
-    commonJs()
-  ]
+const umdConf = {
+  format: 'umd',
+  name: '__my_global_var_name__',
+  banner: `// Version ${version} ${name} - ${homepage}`
 };
+
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        ...umdConf,
+        file: `dist/${name}.js`,
+        sourcemap: true
+      },
+      { // minify
+        ...umdConf,
+        file: `dist/${name}.min.js`,
+        plugins: [terser({
+          output: { comments: '/Version/' }
+        })]
+      }
+    ],
+    plugins: [
+      postCss({
+        plugins: [
+          postCssSimpleVars(),
+          postCssNested()
+        ]
+      }),
+      babel({ exclude: 'node_modules/**' }),
+      resolve(),
+      commonJs()
+    ]
+  },
+  { // commonJs and ES modules
+    input: 'src/index.js',
+    output: [
+      {
+        format: 'cjs',
+        file: `dist/${name}.common.js`
+      },
+      {
+        format: 'es',
+        file: `dist/${name}.module.js`
+      }
+    ],
+    external: Object.keys(dependencies),
+    plugins: [
+      postCss({
+        plugins: [
+          postCssSimpleVars(),
+          postCssNested()
+        ]
+      }),
+      babel()
+    ]
+  }
+];
